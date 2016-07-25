3ffe2f75467eb302d2b92ab85addbfe70fa95b9f
Tim <timmay3000@sbcglobal.net>
2016-07-25 14:31:10 -0400

    Added classes.

--- 07 Classes With Properties/Program.cs
+++ 07 Classes With Properties/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace _07_Classes_With_Properties
 {
+	public class Politician
+	{
+		public string Name { get; set; }
+		public string Party { get; set; }
+		public string RunningFor { get; set; }
+		public override string ToString()
+		{
+			return $"{Name} is a {Party} who is running for {RunningFor}.";
+		}
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var banks = new Politician();
+			banks.Name = "Jim Banks";
+			banks.Party = "Republican";
+			banks.RunningFor = "Congress";
+			Console.WriteLine(banks.ToString());
+			Console.ReadLine();
 		}
 	}
 }
--- 10 Classes With Constructors/Program.cs
+++ 10 Classes With Constructors/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace _10_Classes_With_Constructors
 {
+	public class Player
+	{
+		public Player(string name, string sport, string team, string position, int number)
+		{
+			Name = name;
+			Sport = sport;
+			Team = team;
+			Position = position;
+			Number = number;
+
+		}
+
+		public string Name { get; set; }
+		public string Sport { get; set; }
+		public string Team { get; set; }
+		public string Position { get; set; }
+		public int Number { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Name} - {Number}";
+		}
+
+		public string writeInfo()
+		{
+			return $"{Name} is a {Position} who plays for the {Team}. {Name}'s number is {Number}.";
+		}
+	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var antonioBrown = new Player("Antonio Brown", "Football", "Steelers", "wide receiver", 84);
+			Console.WriteLine(antonioBrown.writeInfo());
+			Console.ReadLine();
 		}
 	}
 }
